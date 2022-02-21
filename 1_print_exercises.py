99d7f535f84319abfd8d6d33dbb4b753c7bb03f0
SuggG2 <hammce16@kingshigh.school.nz>
2022-02-22 12:14:21 +1300

    Did my first exercise.
    printing random stuff

---
+++ 1_print_exercises.py
@@ -0,0 +1,22 @@
+#1 print out name
+print("Hamish")
+#2 print out teachers name
+print("Luke Smith")
+#3 print out the name of your school
+print("Kings High School")
+#4 print out your age
+print("15")
+#5 print most hours played on a game
+print("GTAV 1,608.2 hours")
+#6 print out what the year will be in 25 years
+print(2022+25)
+#7 print out how old you will be in 100 years
+print(15+100)
+#8 print 658 divided by 7
+print(658/7)
+#9 print how many years ago the moon landing 
+print(2022-1969)
+#10 print out the name of you favourite game
+print("GMod")
+#11 print out your favourite class
+print("History")
