7b9f2220f0c431eaa7e2ff63772ea3effcfe764d
SuggG2 <hammce16@kingshigh.school.nz>
2022-02-24 14:55:11 +1300

    Create 2_data_types.py

---
+++ 2_data_types.py
@@ -0,0 +1,40 @@
+#1 
+print("Annie, are you OK\nWill you tell us that you're OK\nThere's a sign at the window\nThat he struck you\nHe came into your apartment\nHe left the bloodstains on the carpet\nThen you ran into the bedroom\nYou were struck down\nIt was your doom\n")
+#2
+print("C’est banana! Hahaha! Miam Miam! Huh?\n")
+#3
+print("a string is text that cant be multiplied or added to\n")
+#4
+print("An integer is a similar to a string but in numerical form that can be added to or subtracted from\n")
+#5
+print("A float is  a number with a decimal point\n")
+#6
+print(543/3)
+#7
+print(543//3)
+#8
+print(543%3)
+#9
+print(1000%9)
+#10
+print(434//10)
+#11
+print(3%2)
+#12
+print(5%3)
+#13
+print(555/2)
+#14
+print(123/2)
+#15
+print(123//2)
+#16
+print(123%2)
+#17
+print(764%2)
+#18
+print(165//2)
+#19
+print(6.2+6.3)
+#20
+print
