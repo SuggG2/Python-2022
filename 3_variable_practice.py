63b2b090cb7350d73d6af1d99168710fec0c1698
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-01 10:54:25 +1300

    Update 3_variable_practice.py

--- 3_variable_practice.py
+++ 3_variable_practice.py
@@ -1,10 +1,42 @@
 #1
 player_lives = 100
 #2
-player_login = "Big_Crong"
+player_login = "Big crong"
 #3
 player_money = 200
 #4
-player_phone_number = 022 222 2222
+player_phone_number = "022 222 2222"
 #5
-player_name
+player_name = "Big"
+#6
+player_age = 15
+#7
+year = 2022
+#8
+print(player_name)
+#9
+print(player_age)
+#10
+print(year)
+#11
+surname = "Crong"
+#12
+school_name = "Kings High School"
+#13
+fav_food = "Ice"
+#14
+pet_name = "Mixed dog"
+#15
+chocolate_amount = 2500
+#16
+score = 100
+#17
+highscore = 505
+#18
+laps_completed = 10
+#19
+ammo_left = 35
+#20
+gear = 1
+#21
+speed = 55
