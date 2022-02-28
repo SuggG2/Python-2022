b42778a7a7b4efe0ff0c199c183646aa7d215bef
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-01 10:54:33 +1300

    Create 4_variable.py

---
+++ 4_variable.py
@@ -0,0 +1,15 @@
+#1
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+#2
+print("You have", money,"dollars")
+#3
+print("Buying a hat")
+#4
+money -= hat
+#5
+print(f"You now have {money} left")
