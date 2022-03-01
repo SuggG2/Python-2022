09220a6e9cf779274cce31470d08d6466ed63a46
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-02 12:23:25 +1300

    Create 5_returningvalues.py

---
+++ 5_returningvalues.py
@@ -0,0 +1,7 @@
+#1
+randomkey = input("Enter a random key, then press enter\n")
+#2
+user_name = input("Enter a Username, then press enter\n")
+#3
+user_age = input("Enter your age, then press enter\n")
+#4
