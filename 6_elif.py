04c19b72d5dfc072f9e748d3e910a5d7a92edf36
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-04 14:56:52 +1300

    Create 6_elif.py

---
+++ 6_elif.py
@@ -0,0 +1,5 @@
+user_ice = int(input("How many ice creams would you like\n"))
+if user_ice <= 20:
+	print("We will have those ready in no time")
+else:
+	print("we dont have that many in stock sorry")
