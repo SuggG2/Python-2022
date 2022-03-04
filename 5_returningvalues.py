739d49e63faf713083697c5d9dc8c024ebe7c117
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-04 14:56:55 +1300

    Update 5_returningvalues.py

--- 5_returningvalues.py
+++ 5_returningvalues.py
@@ -1,7 +1,23 @@
 #1
-randomkey = input("Enter a random key, then press enter\n")
+randomkey = input("Enter a random key\n")
 #2
-user_name = input("Enter a Username, then press enter\n")
+user_name = input("Enter a Username\n")
 #3
-user_age = input("Enter your age, then press enter\n")
+print("Type your age")
 #4
+name = input("Enter your name\n")
+#5
+user_age = int(input("Enter your age\n"))
+#6
+fav_movie = input("Enter your favourite movie\n")
+#7
+book = input("Enter your favourite book\n")
+#8
+adjective = input("Enter an adjective\n")
+#9
+noun = input("Enter an noun")
+#10
+verb = input("Enter a verb\n")
+#11
+print(f"wow {fav_movie} is really bad same with {book}")
+
