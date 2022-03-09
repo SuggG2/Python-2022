574f457be077e0e1d52fd908aae88b2ced0407b0
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-10 11:25:31 +1300

    Update 6_elif.py

--- 6_elif.py
+++ 6_elif.py
@@ -24,7 +24,17 @@ if fav_mov == "lord of the rings":
 	print("Ok loser")
 else:
 	print("Ok")
-"""
+
 #5
-nerd = input("Have you heard of Darth Plageuis?\n")
-if nerd == ""
+nerd = input("Have you heard of Darth Plageuis?\n").lower()
+if nerd == "no":
+	print("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself. - Sheev Palpatine or Darth Sidious.")
+else:
+	print("You must be a fan?")
+"""
+#6
+passionofthenerd = input("Who directed Passion of The Christ?").lower()
+if passionofthenerd == "mel gibson":
+	print(f"Correct {passionofthenerd} directed passion of the christ")
+else:
+	print("No lol")
