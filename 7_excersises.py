8bbc13f2b204b669956499eb5e3bde6b54c77f25
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-10 12:24:57 +1300

    Update 7_excersises.py

--- 7_excersises.py
+++ 7_excersises.py
@@ -1,1 +1,23 @@
+#1
 fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+print(fibonacci)
+#2
+fruits = ["apple", "dragonfruit", "plums", "banana", "tangerine"]
+print(fruits)
+#3
+utube = ["Mishaboss2007", "Slugz buns", "Gengar Gaming", "Snoop Dog"]
+#4
+empty = []
+empty.append("Sailing the seas depends on the Helmsman")
+empty.append("Red sun in the sky")
+empty.append("Ghost ship")
+empty.append("pyongyang")
+empty.append("My Terracotta Heart")
+print(empty)
+#5
+books = []
+books.append("Homage to Catalonia")
+books.append("Animal Farm")
+books.append("1984")
+books.append("Minecraft combat handbook")
+books.append("Minecraft build handbook")
