7209defbe266b29706b98b2cd8953d8d457f6df5
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-11 13:24:59 +1300

    Create 8_excersises.py

---
+++ 8_excersises.py
@@ -0,0 +1,10 @@
+toppingcount = 0
+toppingdone = False 
+toppingslist[]
+
+while toppingdone == False:
+	topping = input("What topping would you like? - push enter to finish")
+	if topping == "":
+		print("Order done")
+		toppingdone = True 
+	elif topping in toppings
