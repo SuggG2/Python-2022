033d0b9a9f43e59020f5e14a04656cef69e5dedb
SuggG2 <hammce16@kingshigh.school.nz>
2022-03-16 09:40:25 +1300

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -37,6 +37,9 @@ quarters.item.add(note)
 
 #variables
 current_room = space
+inventory = Bag()
+body_searched = False
+used_keycard = False
 
 #binds
 @when("jump")
@@ -76,6 +79,57 @@ def look():
 		print("You also see:")
 		for item in current_room.items:
 			print(item)
+
+@when("get ITEM")
+@when("take ITEM")
+def get_tem(item):
+	#check if item is in room take it out of room put into inventory other wise tell user ther is no item
+	if item in current_room.items:
+		t = current_room.items.take(item)
+		inventory.add(t)
+		print(f"You pick up the {item}")
+	else:
+		print(f"you dont see a {item}")
+
+
+@when("inventory")
+def check_inventory():
+	print("you are carrying")
+	for item in inventory:
+		print(item)
+
+@when("search body")
+@when("look at body")
+@when("search body")
+def search_body():
+	if current_room == bridge and body_searched == false:
+		print("you search the body an a red keycard falls to the ground")
+		current_room.items.add(keycard)
+		body_searched = True 
+	elif current_room == bridge and body_searched == True:
+		print("you have already searched the body")
+	else:
+		print("there is no body to search here")
+
+@when("use ITEM")
+def use(item):
+	if item == keycard and current_room == bridge:
+		print("you use the keycard and the escpae pods open")
+		print("the esacpe pod stands open to the south")
+		used_keycard = True 
+		bridge_south = escape
+	else:
+		print("you cant use that here")
+
+@when("type code")
+def escape_pod_win():
+	if note in inventory:
+		if current_room == escape :
+			print("you enter the code and escape. you win")
+		else:
+			print("there is no where to enter the code")
+	else:
+		print("You dont have the code. you cant just guess it")
 
 #EVERYTHING GOES ABOVE HERE - DO NOT CHANGE
 #ANYTHING BELOW THIS LINE
